a4fc78e9047de6632569bf25cfb430c5a7d5da79
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2021-11-27 14:02:43 +0100

    hero text

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,7 +1,20 @@
 <template>
   <VueSlickCarousel v-bind="settings">
-    <div v-for="img in heroImg" :key="img.path" class="img">
-      <img :alt="img.path" :src="img.path">
+    <div v-for="img in heroImg" :key="img.path" class="hero">
+      <div class="container">
+        <div class="hero__text">
+          <div class="hero__title">
+            uczucia
+          </div>
+          <div class="hero__subtitle">
+            znowu to poczujesz
+          </div>
+          <button class="primary-button">
+            zobacz moje prace
+          </button>
+        </div>
+      </div>
+      <img :alt="img.path" :src="img.path" class="hero__img">
     </div>
   </VueSlickCarousel>
 </template>
@@ -11,8 +24,8 @@ import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 // optional style for arrows & dots
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
-import hero1 from '@/assets/img/hero1.jpg'
-import hero2 from '@/assets/img/hero2.jpg'
+import hero1 from '@/assets/img/hero2.jpg'
+import hero2 from '@/assets/img/hero1.jpg'
 import hero3 from '@/assets/img/hero3.jpg'
 
 const heroImg = [
@@ -28,7 +41,7 @@ export default {
       hero3,
       heroImg,
       settings: {
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 4000,
         fade: true,
         dots: true,
@@ -40,7 +53,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-
-</style>
