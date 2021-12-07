cdf6c3a8fbf5c4950b255ee1959ef01a43ee7f47
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2021-12-07 19:12:35 +0100

    section title

--- components/home/About.vue
+++ components/home/About.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="about" />
+  <div class="about">
+    <SectionTitle subtitle="sa najwazniejsi" title="opinie moich klientow" />
+  </div>
 </template>
 
 <script>
+import SectionTitle from './SectionTitle.vue'
+
 export default {
-  name: 'About'
+  name: 'About',
+  components: { SectionTitle }
 }
 </script>
-
-<style scoped>
-
-</style>
---
+++ components/home/SectionTitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="section-title">
+    <div class="section-title__title">
+      {{ title }}
+    </div>
+    <div class="section-title__subtitle">
+      {{ subtitle }}
+    </div>
+    <span class="section-title__border" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionTitle',
+  props: {
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    }
+  }
+}
+</script>
