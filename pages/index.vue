afd6f8e4567e22a7205df6170b566f3f4f6f3261
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2021-12-12 18:37:27 +0100

    all files

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Hero />
+    <About />
+    <Testimonials />
+    <Faq />
+    <Contact />
+  </div>
+</template>
+<script>
+
+import Hero from '../components/home/Hero.vue'
+import About from '../components/home/About.vue'
+import Testimonials from '../components/home/Testimonials.vue'
+import Faq from '../components/home/Faq.vue'
+import Contact from '../components/home/Contact.vue'
+
+export default {
+  components: {
+    Contact,
+    Faq,
+    Testimonials,
+    About,
+    Hero
+  }
+}
+</script>
