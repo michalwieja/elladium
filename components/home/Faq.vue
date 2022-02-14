17c60bd8b20f45c7272187cb6b88b9e513f5ed53
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2022-02-14 17:26:37 +0100

    faq font fix

--- components/home/Faq.vue
+++ components/home/Faq.vue
@@ -22,40 +22,40 @@ import FaqExpand from './FaqExpand.vue'
 const faqList = [
   {
     id: 1,
-    question: 'CZY SAMA WYBIERASZ MIEJSCE NA SESJE?',
+    question: 'Czy sama wybierasz miejsce na sesję?',
     answear: 'Tak, mam wiele ładnych sprawdzonych miejscówek. Jestem jednak otwarta na Wasze propozycje.'
   }, {
     id: 2,
-    question: 'CZY O GARDEROBĘ MUSIMY POSTARAĆ SIĘ SAMI?',
+    question: 'Czy o garderobę musimy postarać się sami?',
     answear: 'Posiadam mobilną garderobę dla przyszłych mam oraz dla dzieci w rozmiarze ( 54 cm. Do 130 cm)'
   }, {
     id: 3,
-    question: 'W JAKIM STYLU NAJLEPIEJ UBRAĆ SIĘ NA SESJĘ?',
+    question: 'W jakim stylu najlepiej ubrać się na sesję?',
     answear: 'W takim, jaki nabradziej lubicie i w którym czujecie się sobą!\n' +
       'Przed sesją wysyłąm na maila informacje dotyczące stroju.'
   }, {
     id: 4,
-    question: 'JAK PRZYGOTOWAĆ MIESZKANIE DO NOWORODKOWEJ SESJI DOMOWEJ?',
-    answear: 'Wszystkie onformacje dostaniecie na maila.'
+    question: 'Jak przygotować mieszkanie do noworodkowej sesji domowej?',
+    answear: 'Wszystkie informacje dostaniecie na maila.'
   }, {
     id: 6,
-    question: 'CZY ORGANIZUJESZ SESJE STYLIZOWANE I MINI SESJE?',
+    question: 'Czy organizujesz sesje stylizowane i mini sesje?',
     answear: 'Tak, organizuję sezonowe Mini Sesje (Boże Narodzenie, Wielkanoc, Jesienne)\n' +
       'Jeśli marzy Ci się stylizowana sesja z okazji urodzin dzieciaczka, to oczywiście taką przygotuję:)'
   },
   {
     id: 7,
-    question: 'JAKIM APARATEM FOTOGRAFUJESZ?',
+    question: 'Jakim aparatem fotografujesz?',
     answear: 'Pracuję na sprzęcie firmy Canon'
   },
   {
     id: 8,
-    question: 'JAK DŁUGO CZEKA SIĘ NA ZDJĘCIA?',
+    question: 'Jak długo czeka sie na zdjęcia?',
     answear: 'Zdjęcia w galerii On-line dostaniecie ode mnie w przeciągu dwóch tygodni od wykonania sesji.'
   },
   {
     id: 9,
-    question: 'CZY POSIADASZ STUDIO?',
+    question: 'Czy posiadasz studio?',
     answear: 'Tak, mam małe domowe studio. Jest odpowienie na sesję dziecięce i portrtowe.'
   }
 ]
