feda4dfcfc9e605b8a928fbee7552bc4d8336b77
m.wieja@qarbon.pl <YqFy5aubS9DmBCG>
2022-02-17 17:12:49 +0100

    client only try

--- nuxt.config.js
+++ nuxt.config.js
@@ -58,12 +58,9 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-slick-carousel.js',
     {
       src: '~/plugins/vue2-google-maps.js',
-      ssr: true
-    },
-    {
-      src: '~/plugins/vue-slick-carousel.js',
       ssr: true
     }
   ],
